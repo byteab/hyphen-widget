627b3a2dfb47f714a1edb33ba8b6b796aab3e428
Ehsan sarshar <ehsan.herai@gmail.com>
2022-03-15 19:07:04 +0430

    add ability to hide the app on outside of React

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, {useLayoutEffect, useState} from "react";
 import Home from "./pages/home/Home";
 
-import { Routes, Route } from "react-router-dom";
-import { Auth } from "./pages/auth/Auth";
 
 const App: React.FC = () => {
+
+  const [isWidgetVisible, setWidgetVisibility] = useState(false)
+
+  const toggleHyphenWidgetVisibility = () => {
+    setWidgetVisibility(value => !value)
+  }
+
+  useLayoutEffect(() => {
+    //@ts-ignore
+    window.toggleHyphenWidgetVisibility = toggleHyphenWidgetVisibility
+  }, [])
+
+  if(!isWidgetVisible) return <span/>
+
   return (
-    <Routes>
-      <Route path="/login" element={<Auth />} />
-      <Route path="/" element={<Home />} />
-    </Routes>
+      <Home/>
   );
 };
 
